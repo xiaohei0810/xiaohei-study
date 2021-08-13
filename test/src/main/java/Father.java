ffa5f304cce4ffb1bed2ed311faa70419e0d7807
liangyusheng <dn960810>
2021-08-13 18:52:24 +0800

    test

---
+++ test/src/main/java/Father.java
@@ -0,0 +1,10 @@
+/**
+ * @Author liangyusheng@xiaomi.com
+ * @Date 2021/8/13 6:51 下午
+ * @Version 1.0
+ */
+public class Father {
+    public static void main(String[] args) {
+        System.out.println(true);
+    }
+}
